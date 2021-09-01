91c04727015e6e12eda6342cb9c4502e3c712c59
xpnobug <63234412+xpnobug@users.noreply.github.com>
2021-09-01 12:50:38 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,11 @@ cd docs/.vuepress/dist # 进入生成的文件夹
 
 # deploy to github
 echo 'iscxp.tk' > CNAME
-if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy'
-  githubUrl=git@github.com:xpnobug/vueblog.git
-  git config --global user.name "xpnobug"
-  git config --global user.email "669307582@qq.com"
-else
   msg='来自github action的自动部署'
   githubUrl=https://xpnobug:${GITHUB_TOKEN}git@github.com:xpnobug/vueblog.git
   git config --global user.name "xpnobug"
   git config --global user.email "669307582@qq.com"
-fi
+ 
 git init
 git add -A
 git commit -m "${msg}"
