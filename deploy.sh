404b27c4d80cbcb05d73d6cb73677ba70ec9abdc
xpnobug <63234412+xpnobug@users.noreply.github.com>
2021-09-01 11:29:55 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,24 @@
-
-
+#!/usr/bin/env sh
 # 确保脚本抛出遇到的错误
 set -e
+npm run build # 生成静态文件
+cd docs/.vuepress/dist # 进入生成的文件夹
 
-# 生成静态文件
-npm run docs:build
-
-# 进入生成的文件夹
-cd docs/.vuepress/dist
-
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
+# deploy to github
+echo 'iscxp.tk' > CNAME
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:xpnobug/vueblog.git
+else
+  msg='来自github action的自动部署'
+  githubUrl=https://xpnobug:${GITHUB_TOKEN}git@github.com:xpnobug/vueblog.git
+  git config --global user.name "xpnobug"
+  git config --global user.email "669307582@qq.com"
+fi
 git init
 git add -A
-git commit -m 'deploy'
-
-# 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:xpnobug/vueblog.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github
 
 cd -
+rm -rf docs/.vuepress/dist
