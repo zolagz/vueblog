dd83c572961502273e9e4f20a639283613f4ed3b
xpnobug <63234412+xpnobug@users.noreply.github.com>
2021-09-01 14:39:46 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,38 +1,26 @@
-# 现已通过GitHub Actions自动部署，提交只需 'git push' 即可
-# 若不想使用自动部署，可以直接运行命令 'npm run deploy' 本地自动提交部署
-
+#!/usr/bin/env sh
 # 确保脚本抛出遇到的错误
 set -e
+npm run docs:build # 生成静态文件
+cd docs/.vuepress/dist # 进入生成的文件夹
 
-# 生成静态文件
-npm run docs:build
-
-# 进入生成的文件夹
-cd docs/.vuepress/dist/
-
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
+# deploy to github
+echo 'iscxp.tk' > CNAME
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:xpnobug/vueblog.git
+  git config -g user.name "xpnobug"
+  git config -g user.email "669307582@qq.com"
+else
+  msg='来自github action的自动部署'
+  githubUrl=https://xpnobug:${GITHUB_TOKEN}@github.com/xpnobug/vueblog.git
+  git config -g user.name "xpnobug"
+  git config -g user.email "669307582@qq.com"
+fi
 git init
 git add -A
-git commit -m 'deploy'
-git remote add origin git@github.com:xpnobug/vueblog
-git checkout -b gh-pages
-
-# 如果发布到 https://<USERNAME>.github.io
-#git push -f git@github.com:zpj80231/zpj80231.github.io.git gh-pages
-#git push origin :gh-pages
-git push -u origin gh-pages -f
-
-cd ..
-#git init
-#git remote add origin git@github.com:zpj80231/znote.git
-git add .
-git commit -m 'update docs'
-git push origin master
-
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github
 
 cd -
+rm -rf docs/.vuepress/dist
